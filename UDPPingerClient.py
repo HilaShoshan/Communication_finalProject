a57743f1ded4183d3346d9e8ce015ec726794747
Hila Shoshan <hilashoshan0605@gmail.com>
2021-07-11 19:34:06 +0300

    added report

--- UDPPingerClient.py
+++ UDPPingerClient.py
@@ -1,26 +1,50 @@
 import socket
 import time
+import math
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_addr = ('localhost', 12000)
 sock.settimeout(1)  # wait up to one second for a reply
 
 try:
+
+    # parameters for the final report:
+    minRTT = math.inf
+    maxRTT = - math.inf
+    sumRTT = 0
+    numRTT = 0
+    numLosts = 0
+
     # send 10 pings to the server using UDP (connectionless protocol)
     for i in range(1, 11):
         start = time.time()
-        message = 'Ping #' + str(i) + " " + time.ctime(start)
+        message = 'Ping ' + str(i) + " " + time.ctime(start)
         try:
+
             sent = sock.sendto(message, server_addr)
             print("Sent " + message)
             data, server = sock.recvfrom(4096)
             print("Received " + data)
             end = time.time()
-            elapsed = end - start
-            print("RTT: " + str(elapsed) + " seconds\n")
+            RTT = end - start
+            print("RTT: " + str(RTT) + " seconds\n")
+
+            if RTT < minRTT:
+                minRTT = RTT
+            if RTT > maxRTT:
+                maxRTT = RTT
+            sumRTT += RTT
+            numRTT += 1
+
         except socket.timeout:
-            print("#" + str(i) + " Requested Time out\n")
+            print("Requested Time out\n")
+            numLosts += 1
 
 finally:
+    print("---------------REPORT-----------------")
     print("closing socket")
+    print("Minimum RTT: ", minRTT)
+    print("Maximum RTT: ", maxRTT)
+    print("Average RTT: ", sumRTT/numRTT)
+    print("Packet Loss Rate: ", (numLosts/10)*100)  # in percentage
     sock.close()
--- UDPPingerServer.py
+++ UDPPingerServer.py
@@ -3,6 +3,7 @@
 
 import random
 from socket import *
+import time
 
 # Create a UDP socket
 # Notice the use of SOCK_DGRAM for UDP packets
@@ -12,12 +13,21 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', 12000))
 
 while True:
+
     # Generate random number in the range of 0 to 10
     rand = random.randint(0, 10)
+
     # Receive the client packet along with the address it is coming from
     message, address = serverSocket.recvfrom(1024)
+
     # Capitalize the message from the client
     message = message.upper()
+
+    # Calculate the time difference
+    #now = time.time()
+    #split the massage to get the time ...
+    #differ =
+
     # If rand is less is than 4, we consider the packet lost and do not respond
     if rand < 4:
         continue
