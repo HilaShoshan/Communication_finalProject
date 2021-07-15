9856ca7bc710920b1ec7ca48dde7fb88b1b2d87e
EladVaknin <74238558+EladVaknin@users.noreply.github.com>
2021-07-15 20:13:50 +0300

    start part b

--- part a/UDPPingerServer.py
+++ part a/UDPPingerServer.py
@@ -11,23 +11,31 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)
 
 # Assign IP address and port number to socket
 serverSocket.bind(('', 12000))
+##k =5 
+##X = 0.1
+##counter = 0 
 
 while True:
-
+    
     # Generate random number in the range of 0 to 10
     rand = random.randint(0, 10)
 
     # Receive the client packet along with the address it is coming from
     message, address = serverSocket.recvfrom(1024)
-
+    counter += 1 
     # Capitalize the message from the client
     message = message.upper()
 
     # Calculate the time difference
-    #now = time.time()
-    #split the massage to get the time ...
-    #differ =
-
+    now = time.time()
+    # split the massage to get the time 
+    split_arr = message.split(' ', 2)
+    time_str = split_arr[2]
+    msg_time = time.strptime(time_str)
+    differ = now - msg_time
+    # print ("The different is : " , differ ) 
+   
+    
     # If rand is less is than 4, we consider the packet lost and do not respond
     if rand < 4:
         continue
